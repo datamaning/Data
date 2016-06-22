71adafa980ed9a7f5cf0f7b854c5a09b9b8999d2
datamaning <george_xl@163.com>
2016-06-22 21:16:08 +0800

    add 文本语料库

--- jiebaTest1.py
+++ jiebaTest1.py
@@ -13,3 +13,5 @@ seg_list=jieba.cut("小明1995年毕业于清华大学",cut_all=True)
 print "Full Mode:"," ".join(seg_list)
 seg_list=jieba.cut("小明硕士毕业于中国科学院计算所，后在日本京都大学身照")
 print "/ ".join(seg_list)
+for i in ra(20):
+
