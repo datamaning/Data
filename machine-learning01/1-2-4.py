04995be2fc46e905d3a9cedd83d1a91df4d691e4
datamaning <george_xl@163.com>
2016-06-26 23:36:55 +0800

    add

--- machine-learning01/1-2-4.py
+++ machine-learning01/1-2-4.py
@@ -1,6 +1,25 @@
 import numpy as np
+
 myZero=np.zeros([3,5])
 print myZero
 myOnes=np.ones([3,5])
 print myOnes
+myRand=np.random.rand(3,4)
+print myRand
+
+myEye=np.eye(3)
+print myEye
+mymatrix=np.mat([[1,2,3],[4,5,6],[7,8,9]])
+print 10*mymatrix
+print np.sum(mymatrix)
+mymatrix1=np.mat([[1,2,3],[4,5,6],[7,8,9]])
+mymatrix2=1.5*np.ones(3)
+print np.multiply(mymatrix1,mymatrix2)
+mylist=np.mat([[1,2,3],[4,5,6],[7,8,9]])
+print np.power(mylist,2)
+mymatrix3=np.mat([[1],[2],[3]])
+print mymatrix1*mymatrix3
+
+print mymatrix1.T
+print mymatrix1.transpose()
 
